080bf952f7251f8e75270a6efeae2d51a415625d
Martin Georgiev <martin_90@abv.bg>
2021-09-04 22:57:09 +0300

    dependency removed

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,5 @@ import Header from "./header/Header";
 import ThreeColumnLayout from "./three-column-layout/ThreeColumnLayout";
 import Question from "./question/Question";
 import Timer from "./timer/Timer";
-import ThemeSwitch from "./theme-switch/ThemeSwitch";
 
-export { Footer, Header, ThreeColumnLayout, Question, Timer, ThemeSwitch };
+export { Footer, Header, ThreeColumnLayout, Question, Timer };
