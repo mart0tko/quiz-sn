3be3af3ab8fc408bfbd979d68115975c8324f903
Martin Georgiev <martin_90@abv.bg>
2021-09-27 17:07:54 +0300

    API removed from the project

--- node-api/router.js
+++
@@ -1,8 +0,0 @@
-const express =  require('express');
-const controller = require('./controller')
-
-const router = express.Router();
-// All API routes
-router.get('/api/quiz/:id', controller.getQuiz);
-
-module.exports = router;
